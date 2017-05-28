c31d9a85e0d1ce191a7418dd54f536e9d71cd5f1
kaifis <kaifer741@gmail.com>
2017-05-28 22:57:37 +0200

    suport for strange machines - /usr/bin/env python3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="aisnakes",
-    version="1.0",
+    version="1.4",
     scripts=["aisnakes", "config.py", "brains.py", "snakes.py"],
     install_requires=["numpy", "arcade"],
 
@@ -10,5 +10,6 @@ setup(
     author_email="kaifer741@gmail.com",
     description="Snakes learning to live with one vs all perceptrons",
     keywords=["snake", "ai", "perceptron", "machine", "learning", "cool", "useless"],
-    url="https://github.com/JanKaifer/AISnakes"
+    url="https://github.com/JanKaifer/AISnakes",
+    download_url="https://github.com/JanKaifer/AISnakes/tarball/master"
 )
